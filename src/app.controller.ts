3313557cdfe7732f9d24f6b788de25d30b5744e0
Zuza Zuber <zuberzuzanna@gmail.com>
2023-04-02 14:54:21 +0200

    remove useless functions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,17 +6,6 @@ import { PurchaseTokensDto } from 'dtos/PurchaseTokensDto';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get('/token-contract-address')
-  // getTokenContractAddress(): { address: string } {
-  //   return { address: this.appService.getTokenContractAddress() };
-  // }
-
-
-  // @Get('/transaction-status')
-  // async getTransactionStatus(@Query('hash') hash: string): Promise<string> {
-  //   return this.appService.getTransactionStatus(hash);
-  // }
-
   @Post('/purchase-tokens')
   async purchaseTokens(@Body() body: PurchaseTokensDto) {
     return {
