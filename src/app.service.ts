034d28fdbe3aed93d2e3701ea0a468b8218cd1b1
Zuza Zuber <zuberzuzanna@gmail.com>
2023-04-02 14:53:44 +0200

    remove contract addresses

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,8 +4,8 @@ import { ethers } from 'ethers';
 import * as tokenJson from '/assets/CasinoToken.json';
 import * as casinoJson from '/assets/Casino.json';
 
-const TOKEN_CONTRACT_ADDRESS = '0x7a511e39a98f2A9fe50995C32a4fCD4Ea17113C5';
-const CASINO_CONTRACT_ADDRESS = '0xF63e52D63D0cE2b4EF146E280DF825306f05D0F5';
+const TOKEN_CONTRACT_ADDRESS = '';
+const CASINO_CONTRACT_ADDRESS = '';
 
 @Injectable()
 export class AppService {
